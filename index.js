20a10f923e9387d33f88ac5e47d5840961b62746
harleyjj <harleyyesm@gmail.com>
2018-05-08 09:36:43 -0700

    compute average with forEach added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function max(numbers) {
   let index = 1;
   let currentMax = numbers[0];
@@ -77,6 +79,60 @@ function testEmpty(fn) {
     testEmpty(min),
   ];
 
+  const numPassing = testResults.filter(function(result) {
+    return result;
+  }).length;
+  console.log(numPassing + ' out of ' + testResults.length + ' tests passing.');
+})();
+
+
+function average(numbers) {
+  let total = 0;
+  numbers.forEach(function(element) {
+    total += element;
+  }, total);
+  return total/numbers.length;
+}
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
+
+// tests
+
+function testFunctionWorks(fn, input, expected) {
+  if (fn(input) === expected) {
+    console.log('SUCCESS: `' + fn.name + '` works on `[' + input + ']`');
+    return true;
+  } else {
+    console.log(
+      'FAILURE: `' +
+        fn.name +
+        '([' +
+        input +
+        '])` should be ' +
+        expected +
+        ' but was ' +
+        fn(input)
+    );
+    return false;
+  }
+}
+
+(function runTests() {
+  const numList1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const correctAns1 = 5.5;
+  const numList2 = [0, -1, 1];
+  const correctAns2 = 0;
+
+  const testResults = [
+    testFunctionWorks(average, numList1, correctAns1),
+    testFunctionWorks(average, numList2, correctAns2),
+  ];
   const numPassing = testResults.filter(function(result) {
     return result;
   }).length;
